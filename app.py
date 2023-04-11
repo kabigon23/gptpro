66d83c6485510db7fd4326d3df9f97bb501272b1
kabigon23 <kabigond23@gmail.com>
2023-04-11 18:45:16 +0900

    v0.0.3

--- app.py
+++ app.py
@@ -12,21 +12,32 @@ def index():
         weather = request.form["weather"]
         mood = request.form["mood"]
         content = request.form["content"]
+        image_prompt = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+        {"role": "user", "content": "'반에서 1등했다'한 강아지를 관계대명사를 이용해서 영어로 바꿔줘"},
+        {"role": "assistant", "content": "My dog, who came in first in his class"},
+        {"role": "user", "content": f"'{content}'한 강아지를 관계대명사를 이용해서 영어로 바꿔줘"},
+        ])
+        print(image_prompt)
         image = openai.Image.create(
-            prompt=f"{mood}, pixel art",
+            prompt=f"{image_prompt}",
             n=1,
-            size="1024x1024"
+            size="256x256"
         )
+        print(image)
         title = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
         {"role": "user", "content": f"'날씨:{weather},기분:{mood},사건:{content}'의 내용으로 쓸 일기의 제목을 유머러스하게 적어줘. "},
-    ])
+        ])
+        print(title)
         contents = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
         {"role": "user", "content": f"'날씨:{weather},기분:{mood},사건:{content}' 의 내용으로 5줄로 일기를 적어줘"},
-    ])
+        ])
+        print(contents)
         return render_template("result.html", title=title.choices[0].message.content, contents=contents.choices[0].message.content, image_url = image['data'][0]['url'])
     return render_template("write.html")
 
